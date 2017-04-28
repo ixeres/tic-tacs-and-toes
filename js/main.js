a6142d4b98deb82e00162214a94d38be04c18ebd
Devin Leduc <ixeres@gmail.com>
2017-04-28 15:18:50 -0400

    Attempting to plan out js functions.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,39 @@ $(function() {
 
 function attachListeners() {}
 
-function doTurn() {}
+var turn = 0
 
-function player() {}
+function doTurn() {
+  turn ++
+  updateState()
+  checkWinner()
+}
 
-function updateState() {}
+function player() {
+  if (turn % 2 = 0) {
+    "X"
+  } else {
+    "O"
+  }
+}
 
-function checkWinner() {}
+function updateState() {
+  player()
+}
+
+function checkWinner() {
+  // Checks if X or O is present in any of the following configurations:
+  // Top row: 00, 01, 02
+  // Middle row: 10, 11, 12
+  // Bottom row: 20, 21, 22
+  //
+  // Left column: 00, 10, 20
+  // Middle column: 01, 11, 21
+  // Right column: 02, 12, 22
+  //
+  // Diagonal upper left: 00, 11, 22
+  // Diagonal upper right: 02, 11, 20
+}
 
 function message() {}
 
