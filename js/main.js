885995a9364e4f492ad093777bef7ea452335f97
Devin Leduc <ixeres@gmail.com>
2017-05-01 10:11:58 -0400

    Still not working, but closer than before.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,150 @@
 $(function() {
 
-function attachListeners() {}
+  var player1Name="" , player2Name="", turn = "";
+  var grid =  [[0,0,0],[0,0,0],[0,0,0]];
+  var hasWinner = 0, moveCount=0;
 
-var turn = 0
+  // Setting variables for player names, turn count, grid markup, winner check, number of moves.
 
-function doTurn() {
-  turn ++
-  updateState()
-  checkWinner()
-}
-
-function player() {
-  if (turn % 2 = 0) {
-    "X"
-  } else {
-    "O"
+  function boardMsg(x){
+      return $("#board").text(x);
   }
+
+  // Relays messages from the board state.
+
+  function setTurn(){
+    var r = Math.floor((Math.random() * 2) + 1);
+    hasWinner=0;
+    if(r==1){
+        turn = player1Name;
+        boardMsg(player1Name+"'s turn now!");
+    }
+    else{
+        turn = player2Name;
+        boardMsg(player2Name+"'s turn now!");
+    }
 }
 
-function updateState() {
-  player()
+// Sets and controls player turns.
+
+function init(){
+        turn = "";
+        grid =  [[0,0,0],[0,0,0],[0,0,0]];
+        boardMsg("");
+        $(".col").map(function() {
+            $(this).text("");
+        }).get();
+        hasWinner = 0;
+        moveCount=0;
 }
 
-function checkWinner() {
-  // Checks if X or O is present in any of the following configurations:
-  // Top row: 00, 01, 02
-  // Middle row: 10, 11, 12
-  // Bottom row: 20, 21, 22
-  //
-  // Left column: 00, 10, 20
-  // Middle column: 01, 11, 21
-  // Right column: 02, 12, 22
-  //
-  // Diagonal upper left: 00, 11, 22
-  // Diagonal upper right: 02, 11, 20
+// Initializes the board.
+
+$("#playButton").click(function (){
+
+    if(hasWinner==1){
+        init();
+    }
+
+    player1Name = $("#player-1-inp").val();
+    player2Name = $("#player-2-inp").val();
+
+    if(player1Name=="" || player2Name==""){
+        alert("Gotta give me some names!");
+        return;
+    }
+
+    setTurn();
+});
+
+$(".col").click(function (){
+
+    if(player1Name=="" || player2Name==""){
+        alert("Gimme your name!");
+        return;
+    }
+
+    var row = $(this).parent().index();
+    var col = $(this).index();
+
+    if(grid[row][col]!== 0){
+        alert("... I'm afraid I can't do that, Dave." );
+        return;
+    }
+
+    if(hasWinner==1){
+        alert("Please play again... Jabroni.");
+        return;
+    }
+
+    if(turn==player1Name){
+        moveCount++;
+        $(this).text("O");
+        grid[row][col] = 1;
+        var ifWon = winnerCheck(1,player1Name);
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMsg("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player2Name;
+                boardMsg(player2Name+"'s turn now!");
+            }
+            return;
+        }
+        else{
+            return;
+        }
+    }
+    else if(turn==player2Name){
+        moveCount++;
+        $(this).text("X");
+        grid[row][col] = 2;
+        var ifWon = winnerCheck(2,player2Name);
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMsg("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player1Name;
+                boardMsg(player1Name+"'s turn now!");
+            }
+            return;
+        }
+        else{
+            return;
+        }
+    }
+});
+
+function winnerCheck(n,playerName){
+    if(
+
+        (grid[0][0]==n && grid[0][1]==n && grid[0][2]==n) ||
+        (grid[1][0]==n && grid[1][1]==n && grid[1][2]==n) ||
+        (grid[2][0]==n && grid[2][1]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][0]==n && grid[2][0]==n) ||
+        (grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
+        (grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
+        (grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
+
+
+        ){
+        boardMsg(playerName+" won the game!");
+        hasWinner = 1;
+        moveCount=0;
+        $("#playButton").text("Play again");
+        return true;
+    }
+    return false;
 }
-
-function message() {}
-
-
 })
